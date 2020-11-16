49f9e870426b31e66fef2067c76503505e239d11
jarre <jarredgriffith29@gmail.com>
2020-11-15 22:36:43 -0800

    Week 1 Assignment

---
+++ Week1/Index.js
@@ -0,0 +1,26 @@
+var candyPrice  = 2;
+var burgerPrice =  10.99; 
+
+var walletDollarAmount = 50;
+var walletCentAmounrt = .10;
+
+var numberOfCloseFriends = 2;
+var totalNumberOfFriends = 20;
+
+var age = 26;
+var ageInMonths = 312;
+
+var firstName = "Jarred";
+var firstNamePreferred = "Jay";
+
+var lastname = "Kilbride";
+var lastNamePrefredded = "Griffith";
+
+var middleNameInitial = "M";
+var middleName = "Mitch";
+
+var totalWalletAmount = (walletDollarAmount + walletCentAmounrt); //Adding total wallet Amount
+
+console.log("Total amount in wallet after perchasing burger is " + (totalWalletAmount - burgerPrice)); 
+console.log("Avg of friends made per year is " + age / totalNumberOfFriends); 
+console.log(firstName + " " + middleNameInitial + ", " + lastNamePrefredded);
