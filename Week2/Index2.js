fde2a10cb9c83caa1b8eede4493ac549f7f2ecc4
JarredGriffith <Jarredgriffith29@gmail.com>
2020-11-26 13:46:06 -0800

    Adding Week 2 Folder

---
+++ Week2/Index2.js
@@ -0,0 +1,32 @@
+console.log("Hello World")
+
+// this is If stuff 
+
+var Name = "Jarred" //prompt()
+
+if(Name  == "Jarred") { console.log("Hello," 
++ Name);
+} 
+else if (Name == "Tessa") { console.log("Love you Tessa") ;// you can have Endless ampount of else ifs
+}
+else { console.log("ew, bad name");
+}
+
+/* Loop 
+For, While, Do, 
+
+For of loops 
+For in Loops
+*/ 
+
+let num = 0 ;
+
+for (num == 0; num < 10; num++ ) {
+    console.log(num);
+}
+
+let guys = ["Jarred", "Mike", "Chase"];
+
+for(men in guys) {
+console.log(guys[men]);
+}
