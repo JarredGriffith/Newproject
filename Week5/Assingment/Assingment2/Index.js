11e1cbd2dc1b39383d88842280b6594eee93094b
JarredGriffith <Jarredgriffith29@gmail.com>
2021-01-06 21:25:02 -0800

    Updating

---
+++ Week5/Assingment/Assingment2/Index.js
@@ -0,0 +1,9 @@
+class pet {
+    constructor(name,type){
+        this.name  = name; 
+        this.type = type;
+    }
+    getdes() {
+        return this; `Hello, my name is ${this.name} and i am a ${this.type}`
+    }
+}
