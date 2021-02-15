71dbcddc5bc662c6478281a2c77bf6ba74acd5ee
JarredGriffith <Jarredgriffith29@gmail.com>
2021-02-14 17:18:01 -0800

    Updating

--- Week11/Assignment/GameV2/game.js
+++ Week11/Assignment/GameV2/game.js
@@ -1,6 +1,11 @@
-let origBoard; 
+//game board
+let origBoard;
+
+//the game status, if false it wont let you click cells
+let gameActive = 'false'
+//player, this will change to x
 let player1 = "O";
-const player2 = "X";
+//cells needed to win
 const winCombos = [
     [0, 1, 2],
     [3, 4, 5],
@@ -14,66 +19,94 @@ const winCombos = [
 
 //changes player from x to o
 function handlePlayerChange() {
-//if current player is x then o else x
-player1 = player1 === "X" ? "O" : "X";
+    //if current player is x then o else x
+    player1 = player1 === "X" ? "O" : "X";
 }
 
-
-
-
 //grabs all the cells
-let cells = document.querySelectorAll(".cell"); 
+let cells = document.querySelectorAll(".cell");
 //calling the start game. 
 //getElementByclass('.restart').addEventListener('click',startGame())
 
-startGame(); 
-
-function startGame (){
-    //going to push an array of 9 elements so 1-9
-    origBoard = Array.from(Array(9).keys())
-    //loops through all the cells and clears them then waits for a click.
-    for(let i =0;i<cells.length; i++) {
+function startGame() {
+    //turn the game to a active state
+    gameActive = true
+    //going to push an array of 9 elements (evey cell on the board)
+    origBoard = ["", "", "", "", "", "", "", "", ""];
+    //loops through all the cells and clears any text if any. also starts the game. 
+    for (let i = 0; i < cells.length; i++) {
         cells[i].innerText = '';
-        cells[i].addEventListener('click',turnClick,false); 
+        //this will start the game, and waits for a user to click. 
+        cells[i].addEventListener('click', turnClick, false);
     }
 }
 
 //will track the click
 function turnClick(cell) {
-    turn(cell.target.id,player1)
-    gameval(); 
-    handlePlayerChange() 
+    //this checks if the game is active and if it isnt then it wont start
+    if (!gameActive) {
+        return;
+    }
+    //this call the function that inputs the text and checks if the cell has been clicked before
+    turn(cell.target.id, player1)
+    gameval();
+    handlePlayerChange()
 
 }
 
-function turn(id,player) {
-    origBoard[id] = player; 
+function turn(id, player) {
+    //check to make sure the cell hasn't been clicked.
+    if (origBoard[id] !== '') {
+        return;
+    }
+    //if not click it will move forward and push the X/O into with index of the cell user clicked. 
+    origBoard[id] = player;
+    //submits the text
     document.getElementById(id).innerHTML = player
+    //for texting reasons
     console.log(origBoard)
 
 }
 
-function gameval () {
-let roundstate = false; 
-for(let i =0; i<=7;i++){
-const win = winCombos[i]; 
-console.log(win)
-let a = origBoard[win[0]];
-let b = origBoard[win[1]];
-let c = origBoard[win[2]];
+//function to check if anyone won the game. 
+function gameval() {
 
-if (a === b && b === c) {
-    roundstate = true;
-    break
+    let roundstate = false;
+    //loops though all clicked cells and the winning combos to see if anyone won the game. 
+    for (let i = 0; i <= 7; i++) {
+        const win = winCombos[i];
+        console.log(win)
+        let a = origBoard[win[0]];
+        let b = origBoard[win[1]];
+        let c = origBoard[win[2]];
+
+        // if it find cells blank then it keeps looping
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        //if it finds a match with a winning combo then it stops and makes sure the winning
+        // combos has all mataching inputs like all items must be a x or a o 
+        // if this matching then it set the roundstate to true 
+        if (a === b && b === c) {
+            roundstate = true;
+            break
+        }
+    }
+    //displays winning message if the roundstate is set to true. 
+    if (roundstate) {
+        alert("win")
+        gameActive = false
+        return
+    }
+
+    //this checks if there is a draw by looking to see if there is any blank elements in the origboard. 
+    let roundDraw = !origBoard.includes("");
+    if (roundDraw) {
+        gameActive = false;
+        alert("draw")
+        return;
+    }
 }
-}
-if (roundstate) {
-    alert("win")
-}
-}
-
-
-
-
+startGame();
 //restart button to clear the board and start a game. 
-document.getElementById('restart').addEventListener('click',()=> startGame())
+document.getElementById('restart').addEventListener('click', () => startGame())
