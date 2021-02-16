92af1d78802ffef440d4861ec71b39604c1fa570
JarredGriffith <Jarredgriffith29@gmail.com>
2021-02-15 16:06:18 -0800

    updating

--- Week11/Assignment/GameV2/game.js
+++ Week11/Assignment/GameV2/game.js
@@ -1,6 +1,5 @@
 //game board
 let origBoard;
-
 //the game status, if false it wont let you click cells
 let gameActive = 'false'
 //player, this will change to x
@@ -17,10 +16,16 @@ const winCombos = [
     [2, 4, 6]
 ];
 
+//Displays whos turn it is
+const playerDisplay = document.getElementById('turn-display');
+const currentPlayer = () => `it's ${player1}'s turn`;
+playerDisplay.innerHTML = currentPlayer();
+
 //changes player from x to o
 function handlePlayerChange() {
     //if current player is x then o else x
     player1 = player1 === "X" ? "O" : "X";
+    playerDisplay.innerHTML = currentPlayer();
 }
 
 //grabs all the cells
@@ -49,11 +54,11 @@ function turnClick(cell) {
     }
     //this call the function that inputs the text and checks if the cell has been clicked before
     turn(cell.target.id, player1)
+    //checks to see if anyone won. 
     gameval();
-    handlePlayerChange()
-
 }
 
+//checks on click
 function turn(id, player) {
     //check to make sure the cell hasn't been clicked.
     if (origBoard[id] !== '') {
@@ -61,6 +66,8 @@ function turn(id, player) {
     }
     //if not click it will move forward and push the X/O into with index of the cell user clicked. 
     origBoard[id] = player;
+        //changes player
+        handlePlayerChange()
     //submits the text
     document.getElementById(id).innerHTML = player
     //for texting reasons
@@ -70,16 +77,15 @@ function turn(id, player) {
 
 //function to check if anyone won the game. 
 function gameval() {
-
     let roundstate = false;
+    // blank var to put the winner in for the banner display.
+    let winningPlayer =''; 
     //loops though all clicked cells and the winning combos to see if anyone won the game. 
     for (let i = 0; i <= 7; i++) {
         const win = winCombos[i];
-        console.log(win)
         let a = origBoard[win[0]];
         let b = origBoard[win[1]];
         let c = origBoard[win[2]];
-
         // if it find cells blank then it keeps looping
         if (a === '' || b === '' || c === '') {
             continue;
@@ -89,13 +95,14 @@ function gameval() {
         // if this matching then it set the roundstate to true 
         if (a === b && b === c) {
             roundstate = true;
+            winningPlayer = a;
             break
         }
     }
     //displays winning message if the roundstate is set to true. 
     if (roundstate) {
-        alert("win")
-        gameActive = false
+        gameActive = false;
+        banner(winningPlayer)
         return
     }
 
@@ -103,10 +110,39 @@ function gameval() {
     let roundDraw = !origBoard.includes("");
     if (roundDraw) {
         gameActive = false;
-        alert("draw")
+        banner()
         return;
     }
 }
+
+//creates banner if it was a tie or a draw
+function banner(player) {
+    let roundDraw = !origBoard.includes("");
+    if (roundDraw) {
+        let banner = document.getElementById('banner')
+        let ban = document.createElement('p');
+        ban.className = 'alert alert-danger text-center'
+        ban.id = "ban"
+        ban.innerHTML = `Game ended in a draw`
+        banner.appendChild(ban)
+    } else {
+        let banner = document.getElementById('banner')
+        let ban = document.createElement('p');
+        ban.className = 'alert alert-success text-center'
+        ban.id = "ban"
+        ban.innerHTML = `${player} WINS THE GAME!!`
+        banner.appendChild(ban)
+    }
+}
+
+//removes banner 
+function removeBanner() {
+    let banner = $('#banner')
+    banner.empty()
+}
 startGame();
-//restart button to clear the board and start a game. 
-document.getElementById('restart').addEventListener('click', () => startGame())
+//restart button to clear the board and start a game also removes the banner. 
+document.getElementById('restart').addEventListener('click', () => {
+    startGame();
+    removeBanner()
+})
